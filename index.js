3091c51632a76a5f0736de78d204c57af7b3cd1e
albindevasia <114718268+albindevasia@users.noreply.github.com>
2023-05-17 16:50:46 +0530

    web

--- index.js
+++ index.js
@@ -4,19 +4,18 @@ showSlides();
 function showSlides() {
   var slides = document.getElementsByClassName("slide");
   
-  // Hide all slides
+  
   for (var i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   
-  // Show the next slide
+  
   slideIndex++;
   if (slideIndex > slides.length) {
-    slideIndex = 1; // Start from the first slide
+    slideIndex = 1; 
   }
   slides[slideIndex - 1].style.display = "block";
   
-  // Change slide every 3 seconds
   setTimeout(showSlides, 5000);
 }
 var slideIndex = 0;
@@ -35,22 +34,22 @@ function showSlides() {
   }
   slides[slideIndex - 1].style.display = "block";
   
-  // Change slide every 3 seconds
+
   setTimeout(showSlides, 5000);
 }
 
-// Navigation controls
+
 function plusSlides(n) {
   slideIndex += n;
   showSlides();
 }
 
-// Previous button
+
 document.getElementById("prevBtn").addEventListener("click", function() {
   plusSlides(-1);
 });
 
-// Next button
+
 document.getElementById("nextBtn").addEventListener("click", function() {
   plusSlides(1);
 });
@@ -59,19 +58,5 @@ function toggleDropdown(event, dropdownId) {
   var dropdown = document.getElementById(dropdownId);
   dropdown.style.display = (dropdown.style.display === 'block') ? 'none' : 'block';
 }
-// Smooth scrolling to the target element
-function smoothScroll(target) {
-  const targetElement = document.querySelector(target);
-  window.scrollTo({
-    top: targetElement.offsetTop,
-    behavior: 'smooth'
-  });
-}
 
-// Attach event listener to the contact link
-const contactLink = document.querySelector('nav a[href="#contact"]');
-contactLink.addEventListener('click', e => {
-  e.preventDefault();
-  const target = contactLink.getAttribute('href');
-  smoothScroll(target);
-});
+
